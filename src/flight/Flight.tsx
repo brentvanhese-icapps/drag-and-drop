4905a2ccf7193b0846288ebc91f17577679e8a17
Brent Van Hese <brent.vanhese@icapps.com>
2023-10-04 09:45:16 +0200

    created flight

---
+++ src/flight/Flight.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './flight.css'
+import { flight } from '../types/flight';
+import { useDroppable } from '@dnd-kit/core';
+import Task from './task/Task';
+
+type FlightItemProps = {
+  flight: flight;
+}
+
+function Flight({flight}: FlightItemProps) {
+  const {setNodeRef} = useDroppable({
+    id: flight.flightNumber
+  });
+
+  return (
+    <div className='flight'>
+      <div className='flightNumber'>
+        {flight.flightNumber}
+      </div>
+      <div className='tasks'>
+        {flight.tasks.map((task) => (
+          <div ref={setNodeRef} >
+            {task.team === 'Tasks' ?
+            <Task task={task} key={task.id}/>
+            :
+            <></>
+            }
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Flight
